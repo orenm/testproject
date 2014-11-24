d9dd4af4d6a73f6a30a8e1f43a27bad5a9592c75
oren <oren@localhost.localdomain>
2014-11-24 05:21:46 -0500

    add a boshclient that ignores ejabberd "no realm" response

--- ChatSite/ChatSite/registration/views.py
+++ ChatSite/ChatSite/registration/views.py
@@ -15,8 +15,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-import datetime
-
 @sensitive_post_parameters()
 @csrf_protect
 @never_cache
